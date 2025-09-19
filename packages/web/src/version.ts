9830796d49e84c69ded1b9a6c3948f3f6da86295
Loïc MICHEL <loic.michel@gmail.com>
2025-09-19 23:51:44 +0200

    version bump

--- packages/web/src/version.ts
+++ packages/web/src/version.ts
@@ -6,4 +6,4 @@ declare const process: any;
 export const APP_VERSION: string =
 	(typeof import.meta !== 'undefined' && (import.meta as any).env && (import.meta as any).env.VITE_APP_VERSION) ||
 	(typeof process !== 'undefined' && process.env && process.env.APP_VERSION) ||
-	'0.1.0-dev';
+	'0.4.0-multiplayer';
